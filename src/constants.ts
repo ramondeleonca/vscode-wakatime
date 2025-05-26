6faddb04b9d4d80ec44ded5a1f9d41f056bbc5c6
Ramón de León <82668051+ramondeleonca@users.noreply.github.com>
2025-05-26 13:50:38 -0600

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,13 @@
-export const COMMAND_API_KEY = 'wakatime.apikey';
-export const COMMAND_API_URL = 'wakatime.apiurl';
-export const COMMAND_CONFIG_FILE = 'wakatime.config_file';
-export const COMMAND_DASHBOARD = 'wakatime.dashboard';
-export const COMMAND_DEBUG = 'wakatime.debug';
-export const COMMAND_DISABLE = 'wakatime.disable';
-export const COMMAND_LOG_FILE = 'wakatime.log_file';
-export const COMMAND_PROXY = 'wakatime.proxy';
-export const COMMAND_STATUS_BAR_CODING_ACTIVITY = 'wakatime.status_bar_coding_activity';
-export const COMMAND_STATUS_BAR_ENABLED = 'wakatime.status_bar_enabled';
+export const COMMAND_API_KEY = 'wakatime2.apikey';
+export const COMMAND_API_URL = 'wakatime2.apiurl';
+export const COMMAND_CONFIG_FILE = 'wakatime2.config_file';
+export const COMMAND_DASHBOARD = 'wakatime2.dashboard';
+export const COMMAND_DEBUG = 'wakatime2.debug';
+export const COMMAND_DISABLE = 'wakatime2.disable';
+export const COMMAND_LOG_FILE = 'wakatime2.log_file';
+export const COMMAND_PROXY = 'wakatime2.proxy';
+export const COMMAND_STATUS_BAR_CODING_ACTIVITY = 'wakatime2.status_bar_coding_activity';
+export const COMMAND_STATUS_BAR_ENABLED = 'wakatime2.status_bar_enabled';
 export enum LogLevel {
   DEBUG = 0,
   INFO,
